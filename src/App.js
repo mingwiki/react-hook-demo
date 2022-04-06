18cf11a2702a85a2518357f3b5496a5dc12a9162
mingwiki <mingwiki@gmail.com>
2022-04-06 20:04:57 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,35 +3,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let state
+let state = []
+let index = 0
 function useState(initial) { // useState hooks
-  state = state || initial
+  state[index] = state[index] || initial
+
+  let curIndex = index
   function setState(newState) {
-    state = newState
+    state[curIndex] = newState
+    console.log("state: " + state + " curIndex:" + curIndex)
+    index = 0
     ReactDOM.render(<App />, document.getElementById('root'))
   }
-  return [state, setState]
+  return [state[index++], setState]
 }
-let oldDeps
+
+
 function useEffect(callback, deps) {
-  // console.log(callback)
-  // console.log(deps)
-  const changed = oldDeps ? !oldDeps.every((e, i) => e === deps[i]) : true
+  let oldDeps = state[index]
+  let changed = oldDeps ? !deps.every((e, i) => e === oldDeps[i]) : true
   if (!deps || changed) {
     callback()
-    oldDeps = deps
+    state[index] = deps
   }
+  index++
 }
 
 function App() {
   const [count, setCount] = useState(0);
+  const [hit, setHit] = useState(0);
   useEffect(() => {
-    console.log('useEffect on mount')
-  }, [count]);
+    console.log('useEffect on count')
+  }, [count])
+  useEffect(() => {
+    console.log('useEffect on hit')
+  }, [hit])
   return (
     <div className="App">
       <p>You click {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click it!</button>
+      <p>You hit {hit} times</p>
+      <button onClick={() => setHit(hit + 1)}>Hit it!</button>
     </div>
   );
 }
